ba3ac723e663bb7a5b653c9f96f13405a1c38879
me_madness <p_rangel@abv.bg>
2024-09-12 21:13:34 +0200

    some changes of setting.py file

--- 01. Django Introduction/djangoIntroduction/djangoIntroduction/settings.py
+++ 01. Django Introduction/djangoIntroduction/djangoIntroduction/settings.py
@@ -30,6 +30,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+MY_APPS = [
+    'djangoIntroduction.todo_app',
+]
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -74,9 +79,13 @@ WSGI_APPLICATION = 'djangoInroduction.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "djangoIntroduction",
+        "USER": "postgres",
+        "PASSWORD": "Dragulia131@",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
