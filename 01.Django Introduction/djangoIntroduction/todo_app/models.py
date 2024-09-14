0f04e8f447ddf7b4d8b4dc6b15347ded77f4129e
me_madness <p_rangel@abv.bg>
2024-09-14 18:47:00 +0200

    begin with next part,make the logic for my view

--- 01.Django Introduction/djangoIntroduction/todo_app/models.py
+++ 01.Django Introduction/djangoIntroduction/todo_app/models.py
@@ -4,7 +4,5 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=50,)
-    
     description = models.TextField()
-    
     created_at = models.DateTimeField(auto_now_add=True,)
