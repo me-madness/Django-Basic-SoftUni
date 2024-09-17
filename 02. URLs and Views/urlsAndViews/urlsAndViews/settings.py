8fec5b337f04328d44bb1f91080fd7f854fdf6b2
me_madness <p_rangel@abv.bg>
2024-09-17 20:07:02 +0200

    now my project work correctly

--- 02. URLs and Views/urlsAndViews/urlsAndViews/settings.py
+++ 02. URLs and Views/urlsAndViews/urlsAndViews/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'departments',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,12 +75,15 @@ WSGI_APPLICATION = 'urlsAndViews.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "urlsandviews",
+        "USER": "postgres",
+        "PASSWORD": "Dragulia131@",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
