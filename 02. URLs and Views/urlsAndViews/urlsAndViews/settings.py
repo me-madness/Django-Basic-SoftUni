0f1f6bb2aa660f14c1d48393c79b3f648985d717
me_madness <p_rangel@abv.bg>
2024-09-19 16:52:10 +0200

    I create a new django project for lesson Templates Basics

--- 02. URLs and Views/urlsAndViews/urlsAndViews/settings.py
+++ 02. URLs and Views/urlsAndViews/urlsAndViews/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "urlsandviews",
         "USER": "postgres",
-        "PASSWORD": "Dragulia131@",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
