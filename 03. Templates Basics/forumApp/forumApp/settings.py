13ff13fa591c4a8fd63bcaa2393424a520179f4c
me_madness <p_rangel@abv.bg>
2024-09-19 17:35:28 +0200

    New app is added in my forumApp project folder

--- 03. Templates Basics/forumApp/forumApp/settings.py
+++ 03. Templates Basics/forumApp/forumApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pcq4_cif6zr@b!yen*wx1nkpjy^zkt5_8q@zfwn8$qlic_90gp'
+SECRET_KEY = 'django-insecure-+g9ft0-5_&8ixu#ns7n7i$bg-6+&1d-6+e@9&8=mm264tm4mc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
