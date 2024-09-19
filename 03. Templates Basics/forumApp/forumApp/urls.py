2afa317369eff55297509722a6c4950220591827
me_madness <p_rangel@abv.bg>
2024-09-19 17:17:56 +0200

    I create a new project

--- 03. Templates Basics/forumApp/forumApp/urls.py
+++ 03. Templates Basics/forumApp/forumApp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for templatesBasics project.
+URL configuration for forumApp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- 03. Templates Basics/forumApp/forumApp/wsgi.py
+++ 03. Templates Basics/forumApp/forumApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for templatesBasics project.
+WSGI config for forumApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templatesBasics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forumApp.settings')
 
 application = get_wsgi_application()
