4fcff0713afee3bb4a3a0fe56ba0ab71b9ff4995
me_madness <p_rangel@abv.bg>
2024-09-19 19:06:46 +0200

    now my home page is show the current time

--- 03. Templates Basics/forumApp/posts/views.py
+++ 03. Templates Basics/forumApp/posts/views.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'base.html')
+    
+    context = {
+        "current_time": datetime.now()
+    }
+    return render(request, 'base.html', context)
