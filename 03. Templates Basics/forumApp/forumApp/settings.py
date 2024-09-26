c2fd750730ee40f04054763a6a43436c17cdd0d1
me_madness <p_rangel@abv.bg>
2024-09-26 17:24:15 +0200

    all files are prepare for todays lesson

--- 03. Templates Basics/forumApp/forumApp/settings.py
+++ 03. Templates Basics/forumApp/forumApp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "forumApp.posts"
+    "posts"
 ]
 
 MIDDLEWARE = [
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'forumApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "forumApp",
+        "USER": "postgres",
+        "PASSWORD": "Dragulia131@",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
