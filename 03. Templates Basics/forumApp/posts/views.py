e3d8272838364fee470360dc478fe34db7fb71dc
me_madness <p_rangel@abv.bg>
2024-09-27 18:15:31 +0200

    let's finish this lesson

--- 03. Templates Basics/forumApp/posts/views.py
+++ 03. Templates Basics/forumApp/posts/views.py
@@ -1,6 +1,19 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'base.html')
+    
+    context = {
+        "current_time": datetime.now(),
+        "person": {
+            "age": 20,
+            "height": 190,
+        },
+        "ids": ["658921deer", "1232544dss", "4785swrs55"]
+    }    
+    
+    
+    return render(request, 'base.html', context)
