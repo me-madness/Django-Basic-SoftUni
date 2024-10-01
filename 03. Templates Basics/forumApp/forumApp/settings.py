f60f3248c71624dc418a2272e0f2bd97e9d740b6
me_madness <p_rangel@abv.bg>
2024-10-01 11:48:56 +0200

    all files are in my project folder

--- 03. Templates Basics/forumApp/forumApp/settings.py
+++ 03. Templates Basics/forumApp/forumApp/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "forumApp",
         "USER": "postgres",
-        "PASSWORD": "Dragulia131@",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
