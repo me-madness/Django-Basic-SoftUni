fabc155d6a0ba46d3fbb931abf1eb9da6fbe475f
me_madness <p_rangel@abv.bg>
2024-10-02 18:30:20 +0200

    views.py in photo app is ready to used

--- 04. Workshop01/petstagram/petstagram/photos/views.py
+++ 04. Workshop01/petstagram/petstagram/photos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
 # Create your views here.
+def photo_add_page(request):
+    return render(request, 'photo/photo-add-page.html')
+
+
+def photo_edit_page(request):
+    return render(request, 'photo/photo-edit-page.html')
+                  
+                  
+def photo_details_page(request):
+    return render(request, 'photo/photo-details-page.html')                  
